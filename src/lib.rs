310c73864e19ebd0b01a3c14af4a3f7f4d1139ef
Retropaint <darkglasses1122@gmail.com>
2025-01-25 01:57:34 +0700

    WIP modular animFrame logic structure

--- src/lib.rs
+++ src/lib.rs
@@ -6,13 +6,21 @@ pub use runtime::load_dragon_bones;
 #[cfg(test)]
 mod tests {
     use crate::{
-        load_dragon_bones,
+        animate, load_dragon_bones,
         runtime::{Armature, Root},
     };
 
     #[test]
     fn load_armature() {
-        let r: Root = load_dragon_bones("").expect("");
-        panic!("{}", r.armature[0].animation[0].name);
+        let r: Root =
+            load_dragon_bones("/Users/o/projects/code/rust/rusty_dragonbones/src/gopher_ske.json")
+                .expect("");
+        let test: i32 = 0;
+        animate(&r.armature[0], 0, 150, 60, test, |test, m| {
+            for prop in m {
+                println!("{}", prop.pos.x);
+            }
+        });
+        panic!("");
     }
 }
--- src/runtime.rs
+++ src/runtime.rs
@@ -1,7 +1,7 @@
 use std::{fs::File, io::BufReader};
 
 use serde::Deserialize;
-use serde_json::Value;
+use tween::Tweener;
 
 #[derive(Deserialize)]
 pub struct Frame {
@@ -13,6 +13,7 @@ pub struct Frame {
     pub y: f64,
     #[serde(default)]
     pub rotation: f64,
+    pub duration: i32,
 }
 
 #[derive(Deserialize)]
@@ -40,6 +41,20 @@ pub struct Armature {
 #[derive(Deserialize)]
 pub struct Root {
     pub armature: Vec<Armature>,
+    #[serde(default, rename = "frameRate")]
+    pub frame_rate: i32,
+}
+
+pub struct Vec2 {
+    pub x: f64,
+    pub y: f64,
+}
+
+pub struct Prop {
+    pub pos: Vec2,
+    pub scale: Vec2,
+    pub rot: f64,
+    pub name: String,
 }
 
 pub fn load_dragon_bones(path: &str) -> std::io::Result<Root> {
@@ -51,6 +66,39 @@ pub fn load_dragon_bones(path: &str) -> std::io::Result<Root> {
     Ok(r)
 }
 
-pub fn animate<T>(json: Value, model: T, callback: fn(v: Value, m: T)) {
-    callback(json, model);
+pub fn animate<T>(
+    armature: &Armature,
+    anim_idx: usize,
+    frame: i32,
+    frame_rate: i32,
+    model: T,
+    callback: fn(m: T, p: Vec<Prop>),
+) {
+    let mut props: Vec<Prop> = Vec::new();
+
+    for bone in &armature.animation[anim_idx].bone {
+        animate_frame(&bone.translate_frame, frame, frame_rate, &mut props);
+    }
+
+    callback(model, props);
+}
+
+fn animate_frame(anim_frame: &Vec<Frame>, frame: i32, frame_rate: i32, props: &mut Vec<Prop>) {
+    let frame_idx = get_frame_idx(anim_frame, frame, frame_rate);
+    props.push(Prop {
+        pos: Vec2 {
+            x: (Tweener::linear(0, 20, 600).move_to(frame) as f64) / 10.0,
+            y: 0.0,
+        },
+        scale: Vec2 { x: 0.0, y: 0.0 },
+        rot: 1.0,
+        name: "".to_string(),
+    });
+}
+
+fn get_frame_idx(anim_frame: &Vec<Frame>, frame: i32, frame_rate: i32) {
+    let time: i32 = 0;
+    for f in anim_frame {
+        if frame < (time + f.duration) {}
+    }
 }
