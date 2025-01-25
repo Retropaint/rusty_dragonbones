a6e34e209caf092c14604ce77d35fc9b0817a1f1
Retropaint <darkglasses1122@gmail.com>
2025-01-25 17:56:12 +0700

    moved props one level up and added animate_rotate

--- src/lib.rs
+++ src/lib.rs
@@ -5,22 +5,28 @@ pub use runtime::load_dragon_bones;
 
 #[cfg(test)]
 mod tests {
+    use std::{thread, time::Duration};
+
     use crate::{
         animate, load_dragon_bones,
-        runtime::{Armature, Root},
+        runtime::{Armature, DragonBonesRoot},
     };
 
     #[test]
     fn load_armature() {
-        let r: Root =
+        let r: DragonBonesRoot =
             load_dragon_bones("/Users/o/projects/code/rust/rusty_dragonbones/src/gopher_ske.json")
                 .expect("");
         let test: i32 = 0;
-        animate(&r.armature[0], 0, 150, 60, test, |test, m| {
-            for prop in m {
-                println!("{}", prop.pos.x);
-            }
-        });
-        panic!("");
+        let mut frame: i32 = 0;
+        loop {
+            thread::sleep(Duration::from_millis(100));
+            animate(&r.armature[0], 0, frame, 60, test, |test, m| {
+                for prop in m {
+                    println!("{}", prop.pos.x);
+                }
+            });
+            frame += 1;
+        }
     }
 }
--- src/runtime.rs
+++ src/runtime.rs
@@ -12,7 +12,7 @@ pub struct Frame {
     #[serde(default)]
     pub y: f64,
     #[serde(default)]
-    pub rotation: f64,
+    pub rotate: f64,
     pub duration: i32,
 }
 
@@ -23,13 +23,14 @@ pub struct Bone {
     pub translate_frame: Vec<Frame>,
     #[serde(default, rename = "scaleFrame")]
     pub scale_frame: Vec<Frame>,
-    #[serde(default, rename = "rotationFrame")]
-    pub rotation_frame: Vec<Frame>,
+    #[serde(default, rename = "rotateFrame")]
+    pub rotate_frame: Vec<Frame>,
 }
 
 #[derive(Deserialize)]
 pub struct Animation {
     pub name: String,
+    pub duration: i32,
     pub bone: Vec<Bone>,
 }
 
@@ -39,7 +40,7 @@ pub struct Armature {
 }
 
 #[derive(Deserialize)]
-pub struct Root {
+pub struct DragonBonesRoot {
     pub armature: Vec<Armature>,
     #[serde(default, rename = "frameRate")]
     pub frame_rate: i32,
@@ -57,12 +58,12 @@ pub struct Prop {
     pub name: String,
 }
 
-pub fn load_dragon_bones(path: &str) -> std::io::Result<Root> {
+pub fn load_dragon_bones(path: &str) -> std::io::Result<DragonBonesRoot> {
     let file = File::open(path)?;
     let reader = BufReader::new(file);
     //let de = &mut serde_json::Deserializer::from_reader(reader);
     //let s: Root = serde_path_to_error::deserialize(de).expect("");
-    let r: Root = serde_json::from_reader(reader).expect("");
+    let r: DragonBonesRoot = serde_json::from_reader(reader).expect("");
     Ok(r)
 }
 
@@ -74,31 +75,96 @@ pub fn animate<T>(
     model: T,
     callback: fn(m: T, p: Vec<Prop>),
 ) {
-    let mut props: Vec<Prop> = Vec::new();
+    if frame > armature.animation[anim_idx].duration {
+        return;
+    }
 
+    let mut props: Vec<Prop> = Vec::new();
     for bone in &armature.animation[anim_idx].bone {
-        animate_frame(&bone.translate_frame, frame, frame_rate, &mut props);
+        props.push(Prop {
+            pos: Vec2 { x: 0.0, y: 0.0 },
+            scale: Vec2 { x: 0.0, y: 0.0 },
+            rot: 1.0,
+            name: "".to_string(),
+        });
+        animate_frame(
+            &bone.translate_frame,
+            frame,
+            frame_rate,
+            &mut props.last_mut().unwrap().pos,
+        );
+        animate_frame(
+            &bone.scale_frame,
+            frame,
+            frame_rate,
+            &mut props.last_mut().unwrap().scale,
+        );
+        animate_rotate(
+            &bone.rotate_frame,
+            frame,
+            frame_rate,
+            &mut props.last_mut().unwrap().rot,
+        );
     }
 
     callback(model, props);
 }
 
-fn animate_frame(anim_frame: &Vec<Frame>, frame: i32, frame_rate: i32, props: &mut Vec<Prop>) {
-    let frame_idx = get_frame_idx(anim_frame, frame, frame_rate);
-    props.push(Prop {
-        pos: Vec2 {
-            x: (Tweener::linear(0, 20, 600).move_to(frame) as f64) / 10.0,
-            y: 0.0,
-        },
-        scale: Vec2 { x: 0.0, y: 0.0 },
-        rot: 1.0,
-        name: "".to_string(),
-    });
+fn animate_frame(anim_frame: &Vec<Frame>, frame: i32, frame_rate: i32, vec2_p: &mut Vec2) {
+    let (frame_idx, curr_frame) = get_frame_idx(anim_frame, frame, frame_rate);
+    if frame_idx == -1 {
+        return;
+    }
+
+    // since tweener's move_to() only increments in integers, the values are multiplied and then divided by ampl (amplifier)
+    let ampl: f64 = 10.0;
+
+    *vec2_p = Vec2 {
+        x: (Tweener::linear(
+            anim_frame[frame_idx as usize].x * ampl,
+            anim_frame[(frame_idx + 1) as usize].x * ampl,
+            anim_frame[frame_idx as usize].duration,
+        )
+        .move_to(curr_frame) as f64)
+            / ampl,
+        y: (Tweener::linear(
+            anim_frame[frame_idx as usize].y * ampl,
+            anim_frame[(frame_idx + 1) as usize].y * ampl,
+            anim_frame[frame_idx as usize].duration,
+        )
+        .move_to(curr_frame) as f64)
+            / ampl,
+    }
 }
 
-fn get_frame_idx(anim_frame: &Vec<Frame>, frame: i32, frame_rate: i32) {
-    let time: i32 = 0;
-    for f in anim_frame {
-        if frame < (time + f.duration) {}
+fn animate_rotate(anim_frame: &Vec<Frame>, frame: i32, frame_rate: i32, float_p: &mut f64) {
+    let (frame_idx, curr_frame) = get_frame_idx(anim_frame, frame, frame_rate);
+    if frame_idx == -1 {
+        return;
     }
+
+    // since tweener's move_to() only increments in integers, the values are multiplied and then divided by ampl (amplifier)
+    let ampl: f64 = 10.0;
+
+    *float_p = Tweener::linear(
+        anim_frame[frame_idx as usize].rotate * ampl,
+        anim_frame[(frame_idx + 1) as usize].rotate * ampl,
+        anim_frame[frame_idx as usize].duration,
+    )
+    .move_to(curr_frame) as f64
+        / ampl;
+}
+
+// returns current anim frame, as well as the frame of it
+fn get_frame_idx(anim_frame: &Vec<Frame>, frame: i32, frame_rate: i32) -> (i32, i32) {
+    let mut time: i32 = 0;
+    let mut i: i32 = 0;
+    for f in anim_frame {
+        if frame < (time + f.duration) {
+            return (i, frame - time);
+        };
+        time += f.duration;
+        i += 1;
+    }
+    (-1, -1)
 }
