69b051a277a62c6445e7dcc1b00b12ebfaa099ed
Retropaint <darkglasses1122@gmail.com>
2025-01-27 00:35:10 +0700

    added clone trait to structs, removed normalize_frames, and made props inherit from parents by default

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,20 @@
+#[macro_use]
 pub mod runtime;
 
 #[cfg(test)]
 mod tests {
     use std::{thread, time::Duration};
 
-    use crate::runtime::{animate, load_dragon_bones, DragonBonesRoot};
+    use crate::runtime::{animate, load_dragon_bones, DragonBonesRoot, Vec2};
 
     #[test]
     fn load_armature() {
         let mut r: DragonBonesRoot =
             load_dragon_bones("/Users/o/projects/code/rust/rusty_dragonbones/src/gopher_ske.json")
                 .expect("");
+        let mut props = animate(&mut r, 0, 30, 0);
+        println!("{} {} {}", props[0].rot, props[0].pos.x, props[0].scale.x);
         let mut test: f64 = 0.0;
-        let props = animate(&mut r, 0, 0, 60);
-        println!("{}", props[1].pos.x);
-        let mut frame: i32 = 0;
-        return;
-        loop {
-            thread::sleep(Duration::from_millis(100));
-            frame += 1;
-        }
+        let mut props = animate(&mut r, 0, 0, 60);
     }
 }
--- src/runtime.rs
+++ src/runtime.rs
@@ -3,20 +3,20 @@ use std::{fs::File, io::BufReader};
 use serde::Deserialize;
 use tween::Tweener;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 pub struct Frame {
     #[serde(default, rename = "tweenEasing")]
     pub tween_easing: f64,
-    #[serde(default = "transform_default")]
+    #[serde(default)]
     pub x: f64,
-    #[serde(default = "transform_default")]
+    #[serde(default)]
     pub y: f64,
-    #[serde(default = "transform_default")]
+    #[serde(default)]
     pub rotate: f64,
     pub duration: i32,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 pub struct AnimBone {
     pub name: String,
     #[serde(default, rename = "translateFrame")]
@@ -27,7 +27,7 @@ pub struct AnimBone {
     pub rotate_frame: Vec<Frame>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 pub struct Bone {
     pub name: String,
     #[serde(default)]
@@ -35,58 +35,76 @@ pub struct Bone {
     pub transform: Transform,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 pub struct Transform {
     #[serde(default)]
     pub x: f64,
     #[serde(default)]
     pub y: f64,
 
-    // this will probably backfire later
+    /// this will probably backfire later
     #[serde(default, rename = "skX")]
     pub rot: f64,
 
-    #[serde(default = "scale_default")]
-    pub scX: f64,
-    #[serde(default = "scale_default")]
-    pub scY: f64,
+    #[serde(default = "scale_default", rename = "scX")]
+    pub sc_x: f64,
+    #[serde(default = "scale_default", rename = "scY")]
+    pub sc_y: f64,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 pub struct Animation {
     pub name: String,
     pub duration: i32,
     pub bone: Vec<AnimBone>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 pub struct Armature {
     pub animation: Vec<Animation>,
     pub bone: Vec<Bone>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 pub struct DragonBonesRoot {
     pub armature: Vec<Armature>,
     #[serde(default, rename = "frameRate")]
     pub frame_rate: i32,
 }
 
+#[derive(Clone)]
 pub struct Vec2 {
     pub x: f64,
     pub y: f64,
 }
 
+/// Returned from animate(), providing all animation data of a single bone (and then some) in a single frame.
+#[derive(Clone)]
 pub struct Prop {
+    pub name: String,
+    pub parent_name: String,
+
+    /// index of the parent prop, relative to the vector containing this prop and it.
+    pub parent_idx: i32,
+
     pub pos: Vec2,
     pub scale: Vec2,
     pub rot: f64,
-    pub name: String,
 }
 
-fn transform_default() -> f64 {
-    return 99999.0;
+/// Parameters: (prop1: Prop, prop2: Prop)
+/// Inherits transform values from prop2 into prop1.
+/// Useful for inheriting parent values into its child/children.
+macro_rules! inherit_prop {
+    ($prop1:expr, $prop2:expr) => {
+        $prop1.pos.x += $prop2.pos.x;
+        $prop1.pos.y += $prop2.pos.y;
+        $prop1.scale.x *= $prop2.scale.x;
+        $prop1.scale.y *= $prop2.scale.y;
+        $prop1.rot += $prop2.rot;
+    };
 }
+
 fn scale_default() -> f64 {
     return 1.0;
 }
@@ -96,43 +114,11 @@ pub fn load_dragon_bones(path: &str) -> std::io::Result<DragonBonesRoot> {
     let reader = BufReader::new(file);
     //let de = &mut serde_json::Deserializer::from_reader(reader);
     //let s: Root = serde_path_to_error::deserialize(de).expect("");
-    let mut r: DragonBonesRoot = serde_json::from_reader(reader).expect("");
-    normalize_frames(&mut r.armature[0]);
+    let r: DragonBonesRoot = serde_json::from_reader(reader).expect("");
     Ok(r)
 }
 
-// Add back missing transform values in anim frames, using their initial ones
-fn normalize_frames(armature: &mut Armature) {
-    for a in &mut armature.animation {
-        let mut bi = 0;
-        for b in &mut a.bone {
-            for f in &mut b.translate_frame {
-                if f.x == transform_default() {
-                    f.x = armature.bone[bi].transform.x;
-                }
-                if f.y == transform_default() {
-                    f.y = armature.bone[bi].transform.y;
-                }
-            }
-            for f in &mut b.scale_frame {
-                if f.x == transform_default() {
-                    f.x = armature.bone[bi].transform.scX
-                }
-                if f.y == transform_default() {
-                    f.y = armature.bone[bi].transform.scY
-                }
-            }
-            for f in &mut b.rotate_frame {
-                if f.rotate == transform_default() {
-                    f.rotate = armature.bone[bi].transform.rot;
-                }
-            }
-            bi += 1;
-        }
-    }
-}
-
-// Animate Dragon Bones armature with the specified animation and frame data.
+/// Animate dragon bones armature with the specified animation and frame data.
 pub fn animate(
     root: &mut DragonBonesRoot,
     anim_idx: usize,
@@ -149,27 +135,59 @@ pub fn animate(
                 y: root.armature[0].bone[bi].transform.y,
             },
             scale: Vec2 {
-                x: root.armature[0].bone[bi].transform.scX,
-                y: root.armature[0].bone[bi].transform.scY,
+                x: root.armature[0].bone[bi].transform.sc_x,
+                y: root.armature[0].bone[bi].transform.sc_y,
             },
             rot: root.armature[0].bone[bi].transform.rot,
+
             name: bone.name.to_string(),
+            parent_name: root.armature[0].bone[bi].parent.clone(),
+            parent_idx: prop_by_name(root.armature[0].bone[bi].parent.clone(), &props),
         });
+
+        // animate transforms
         if bone.translate_frame.len() > 0 {
-            props.last_mut().unwrap().pos = animate_frame(&bone.translate_frame, frame, frame_rate);
+            let pos = animate_vec2(&bone.translate_frame, frame, frame_rate);
+            props.last_mut().unwrap().pos.x += pos.x;
+            props.last_mut().unwrap().pos.y += pos.y;
         }
         if bone.scale_frame.len() > 0 {
-            props.last_mut().unwrap().scale = animate_frame(&bone.scale_frame, frame, frame_rate);
+            let scale = animate_vec2(&bone.scale_frame, frame, frame_rate);
+            props.last_mut().unwrap().scale.x *= scale.x;
+            props.last_mut().unwrap().scale.y *= scale.y;
         }
         if bone.rotate_frame.len() > 0 {
-            props.last_mut().unwrap().rot = animate_rotate(&bone.rotate_frame, frame, frame_rate);
+            props.last_mut().unwrap().rot += animate_float(&bone.rotate_frame, frame, frame_rate);
         }
+
+        // inherit transform from parent
+        let this_prop = props.last_mut().unwrap().clone();
+        if this_prop.parent_name != "" {
+            inherit_prop!(
+                props.last_mut().unwrap(),
+                props[this_prop.parent_idx as usize]
+            );
+        }
+
         bi += 1;
     }
     props
 }
 
-fn animate_frame(anim_frame: &Vec<Frame>, frame: i32, frame_rate: i32) -> Vec2 {
+/// Return index of prop with this name.
+fn prop_by_name(name: String, props: &Vec<Prop>) -> i32 {
+    let mut i = 0;
+    for p in props {
+        if p.name == name {
+            return i;
+        }
+        i += 1;
+    }
+    return -1;
+}
+
+/// Animate a frame that returns a Vec2.
+fn animate_vec2(anim_frame: &Vec<Frame>, frame: i32, frame_rate: i32) -> Vec2 {
     let (frame_idx, curr_frame) = get_frame_idx(anim_frame, frame, frame_rate);
 
     // give values directly if this is either the only frame, or it's over
@@ -180,28 +198,24 @@ fn animate_frame(anim_frame: &Vec<Frame>, frame: i32, frame_rate: i32) -> Vec2 {
         };
     }
 
-    // since tweener's move_to() only increments in integers, the values are multiplied and then divided by ampl (amplifier)
-    let ampl: f64 = 10.0;
-
     Vec2 {
         x: (Tweener::linear(
-            anim_frame[frame_idx as usize].x * ampl,
-            anim_frame[(frame_idx + 1) as usize].x * ampl,
+            anim_frame[frame_idx as usize].x,
+            anim_frame[(frame_idx + 1) as usize].x,
             anim_frame[frame_idx as usize].duration,
         )
-        .move_to(curr_frame) as f64)
-            / ampl,
+        .move_to(curr_frame) as f64),
         y: (Tweener::linear(
-            anim_frame[frame_idx as usize].y * ampl,
-            anim_frame[(frame_idx + 1) as usize].y * ampl,
+            anim_frame[frame_idx as usize].y,
+            anim_frame[(frame_idx + 1) as usize].y,
             anim_frame[frame_idx as usize].duration,
         )
-        .move_to(curr_frame) as f64)
-            / ampl,
+        .move_to(curr_frame) as f64),
     }
 }
 
-fn animate_rotate(anim_frame: &Vec<Frame>, frame: i32, _frame_rate: i32) -> f64 {
+/// Animate a frame that returns a float.
+fn animate_float(anim_frame: &Vec<Frame>, frame: i32, _frame_rate: i32) -> f64 {
     let (frame_idx, curr_frame) = get_frame_idx(anim_frame, frame, _frame_rate);
 
     // ditto animate_frame
@@ -209,16 +223,12 @@ fn animate_rotate(anim_frame: &Vec<Frame>, frame: i32, _frame_rate: i32) -> f64 
         return anim_frame.last().unwrap().rotate;
     }
 
-    // ditto animte_frame
-    let ampl: f64 = 10.0;
-
     Tweener::linear(
-        anim_frame[frame_idx as usize].rotate * ampl,
-        anim_frame[(frame_idx + 1) as usize].rotate * ampl,
+        anim_frame[frame_idx as usize].rotate,
+        anim_frame[(frame_idx + 1) as usize].rotate,
         anim_frame[frame_idx as usize].duration,
     )
     .move_to(curr_frame) as f64
-        / ampl
 }
 
 // returns current anim frame, as well as the frame of it
